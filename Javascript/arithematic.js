f4a22e64ebd1b116e7e1d6f4458d2d2c35b33aa0
krishgarg11 <krishgarg045@gmail.com>
2026-04-17 09:20:26 +0530

    hi

---
+++ Javascript/arithematic.js
@@ -0,0 +1,15 @@
+function greet(){
+    console.log("Hello, World!")
+}
+greet();
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(2,3));
+
+const add = (a,b) => a+b;
+console.log(add(2,3)); 
+
+const mul = (a,b) => a*b;
+console.log(mul(3,4));
