55e8ae7c02e6ec9ca98a2ffd5fa4193491f97f56
krishgarg11 <krishgarg045@gmail.com>
2026-05-20 12:18:58 +0530

    hlo

--- exp8/exp8/src/App.jsx
+++ exp8/exp8/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
-function GameScore(){
+import { useState } from "react";
+
+function GameScore() {
   const [score, setScore] = useState(0);
+
   const increment = () => {
     setScore(score + 1);
   };
-}
-const decrement = () => {
+
+  const decrement = () => {
     setScore(score - 1);
-}
-return (
+  };
+
+  return (
     <div>
-        <h1>Game Score: {score}</h1>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+      <h1>Game Score: {score}</h1>
+
+      <button onClick={increment}>Increment</button>
+
+      <button onClick={decrement}>Decrement</button>
     </div>
-);
+  );
+}
+
 export default GameScore;
